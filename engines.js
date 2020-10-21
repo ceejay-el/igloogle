476574661d91e71b7466b6e4c9434074526c6935
ceejay-el <59852540+ceejay-el@users.noreply.github.com>
2020-10-21 15:43:07 -0700

    merge conflicts

--- engines.js
+++ engines.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /**
  # MAIN FUNCTIONS THAT RUN SERVER PROCESSES
  */
@@ -47,4 +48,55 @@ module.exports.results = function(request, response){
         }).catch(function(error){
             console.log("There was an error");
 });
+=======
+/**
+ # MAIN FUNCTIONS THAT RUN SERVER PROCESSES
+ */
+//require dependencies
+require("dotenv").config();
+const express = require("express");
+const axios = require("axios");
+// end of requiring dependencies
+
+
+
+// variable declarations
+const app = express();
+// end of variable declarations
+
+
+/**
+ * initialize middleware
+ */
+app.set("view engine", "ejs");
+// end of initializing middleware
+
+
+
+
+
+/**
+ ### API SEARCH ENGINE RESULTS ======================================================================================= //
+ * 
+ * 
+ */
+// variable declarations
+const params = {
+    access_key: process.env.SERPSTACK_API_KEY,
+    query: "mcdonalds"
+}
+
+
+module.exports.results = function(request, response){
+
+    // load search results from api
+    axios.get("http://api.serpstack.com/search", {params})
+    .then(function(resp){
+        let apiData = resp.data;
+        response.render("results", {SEresults: apiData});
+
+        }).catch(function(error){
+            console.log("There was an error");
+});
+>>>>>>> 2eff32a... working engine
 }
