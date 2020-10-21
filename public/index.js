54227f623b12f275cc119bb7b5dd723f4fae22dd
ceejay-el <59852540+ceejay-el@users.noreply.github.com>
2020-10-20 20:25:38 -0700

    fifth commit

--- public/index.js
+++ public/index.js
@@ -1,10 +1,1 @@
-$("#results-search-form").submit(function (event){
-    event.preventDefault();
-
-    let query = $("#search").val();
-    const apiRequest = process.env.SERPSTACK_API;
-    const api_url = "http://api.serpstack.com/search?access_key=3ca0b573485e1b07e10019e27a0cb5e0&type=web&query=" + query;
-    $.get(api_url, function(data){
-        console.log(data);
-    });
-});
+// serpstack api
