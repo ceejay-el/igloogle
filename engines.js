154503f149e803d0057272928e4e31b9f5fe04e4
ceejay-el <59852540+ceejay-el@users.noreply.github.com>
2020-10-21 01:31:43 -0700

    basic search working

--- engines.js
+++ engines.js
@@ -1,22 +1,50 @@
 /**
- * MAIN FUNCTIONS THAT RUN SERVER PROCESSES
+ # MAIN FUNCTIONS THAT RUN SERVER PROCESSES
  */
 //require dependencies
 require("dotenv").config();
+const express = require("express");
 const axios = require("axios");
+const bodyParser = require("body-parser");
+const { request } = require("express");
 // end of requiring dependencies
 
+
+
 // variable declarations
-const params = {
-    access_key: process.env.SERPSTACK_API_KEY,
-    query: "mcdonalds"
+const app = express();
+// end of variable declarations
+
+
+/**
+ * initialize middleware
+ */
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+// end of initializing middleware
+
+
+
+
+
+/**
+ ### API SEARCH ENGINE RESULTS ======================================================================================= //
+ * 
+ * 
+ */
+module.exports.results = function(request, response){
+    const params = {
+        access_key: process.env.SERPSTACK_API_KEY,
+        query: request.body.search
+    }
+
+    // load search results from api
+    axios.get("http://api.serpstack.com/search", {params})
+    .then(function(resp){
+        let apiData = resp.data;
+        response.render("results", {SEresults: apiData});
+
+        }).catch(function(error){
+            console.log("There was an error");
+});
 }
-
-
-// search results
-// axios.get("https://api.serpstack.com/search", {params})
-//     .then(function(response){
-//         console.log(response.data);
-//         }).catch(function(error){
-//             console.log("There was an error");
-//         });
