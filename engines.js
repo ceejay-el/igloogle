2eff32a7919c736ca316d262fe19dbfbd4af35ed
Chuck Junior <59852540+ceejay-el@users.noreply.github.com>
2020-10-20 23:41:23 -0700

    working engine

--- engines.js
+++ engines.js
@@ -1,11 +1,34 @@
 /**
- * MAIN FUNCTIONS THAT RUN SERVER PROCESSES
+ # MAIN FUNCTIONS THAT RUN SERVER PROCESSES
  */
 //require dependencies
 require("dotenv").config();
+const express = require("express");
 const axios = require("axios");
 // end of requiring dependencies
 
+
+
+// variable declarations
+const app = express();
+// end of variable declarations
+
+
+/**
+ * initialize middleware
+ */
+app.set("view engine", "ejs");
+// end of initializing middleware
+
+
+
+
+
+/**
+ ### API SEARCH ENGINE RESULTS ======================================================================================= //
+ * 
+ * 
+ */
 // variable declarations
 const params = {
     access_key: process.env.SERPSTACK_API_KEY,
@@ -13,10 +36,15 @@ const params = {
 }
 
 
-// search results
-// axios.get("https://api.serpstack.com/search", {params})
-//     .then(function(response){
-//         console.log(response.data);
-//         }).catch(function(error){
-//             console.log("There was an error");
-//         });
+module.exports.results = function(request, response){
+
+    // load search results from api
+    axios.get("http://api.serpstack.com/search", {params})
+    .then(function(resp){
+        let apiData = resp.data;
+        response.render("results", {SEresults: apiData});
+
+        }).catch(function(error){
+            console.log("There was an error");
+});
+}
--- routing.js
+++ routing.js
@@ -1,13 +1,18 @@
 // ROUTING FUNCTIONS
 
+// require express and initialize view engine
+const express = require("express");
+const app = express();
+app.set("view engine", "ejs");
+
 // send home page
 module.exports.homePage = function(request, response){
     response.sendFile(__dirname + "/index.html");
 }
 
 // send results page
-module.exports.serp = function(request, response){
-    response.sendFile(__dirname + "/public/results.html");
+module.exports.searchPage = function(request, response){
+    response.render("results");
 }
 
 module.exports.resultsPage = function(request, response){
